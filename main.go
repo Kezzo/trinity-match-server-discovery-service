0797b389eb7de7081fde086b02853b97fa3077fb
Frank Roeder <3roeder@informatik.uni-hamburg.de>
2018-10-31 23:47:45 +0100

    Basic handling of docker events

--- main.go
+++ main.go
@@ -2,19 +2,24 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"os/exec"
 	"time"
 )
+
+var containerMap = map[string]string{}
 
 func main() {
 	var port = os.Getenv("PORT")
 
 	var network = *newNetwork(port)
 
+	listenDockerEvents()
 	go network.listenUDP()
 	go network.sendByteResponse()
 	go processMessages(&network)
@@ -25,6 +30,53 @@ func main() {
 	for {
 		<-ticker.C
 	}
+}
+
+func listenDockerEvents() {
+	cmd := exec.Command("docker", "events", "--filter", "event=start", "--format",
+		"'{{json .}}'")
+	bytesStream := &bytes.Buffer{}
+	cmd.Stdout = bytesStream
+	err := cmd.Start()
+	if err != nil {
+		os.Stderr.WriteString(err.Error())
+	}
+	ticker := time.NewTicker(time.Second)
+	go func(ticker *time.Ticker) {
+		for range ticker.C {
+			streamString := bytesStream.String()
+			var raw map[string]interface{}
+			if streamString != "" {
+				cleanString := streamString[1 : len(streamString)-2]
+				err = json.Unmarshal([]byte(cleanString), &raw)
+				if err != nil {
+					log.Panic(err)
+				}
+				containerID, ok := raw["id"].(string)
+				if ok == false {
+					log.Panic("Different type expected")
+				}
+				if containerMap[containerID] == "" {
+					containerMap[containerID] = getContainerPort(containerID)
+					// Send req to web server
+				}
+				cmd.Stdout.Write([]byte{})
+			}
+		}
+	}(ticker)
+	cmd.Wait()
+}
+
+func getContainerPort(id string) string {
+	log.Println("ID is: ", id)
+	cmd := exec.Command("docker", "inspect",
+		"--format='{{(index (index .NetworkSettings.Ports \"2448/udp\") 0).HostPort}}'", id)
+	out, err := cmd.Output()
+	if err != nil {
+		log.Panic(err)
+	}
+	log.Println("PORT IS", string(out))
+	return string(out)
 }
 
 func processMessages(network *Network) {
